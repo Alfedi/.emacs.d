a7930638447c3e141e42850556fee360201993ca
Alfedi <alfedi3867@hotmail.com>
2018-09-18 12:35:27 +0200

    New changes about cyberpunk-theme

---
+++ cyberpunk-theme-20180609.509/cyberpunk-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cyberpunk-theme" "20180609.509" "Cyberpunk Color Theme" 'nil :commit "f8967e46b8bdb3eaf7b72474f2d70997dc1152e9" :keywords '("color" "theme" "cyberpunk") :authors '(("Nicholas M. Van Horn" . "nvanhorn@protonmail.com")) :maintainer '("Nicholas M. Van Horn" . "nvanhorn@protonmail.com"))
