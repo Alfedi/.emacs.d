08dae80aef5e073ed9b6caa120c4c9da348a2c8e
Alfedi <alfedi3867@hotmail.com>
2018-11-18 14:49:06 +0100

    Changed README and clean init

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-;; Toda la mierda va a ir aquí, maldita configuración custom :(
-(setq custom-file "~/.emacs.d/.emacs-custom.el")
-;; Comprobación de paquetes lo primero. Nos conectamos tanto a melpa como a melpa-stable
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
@@ -10,111 +7,101 @@
    'package-archives
    '("melpa" . "http://melpa.org/packages/"))
   (package-initialize))
-;; Ahora el use-package que agilizará la instalación de nuestros paquetes
+
 (unless (featurep 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
-;; Es el momento de hacerlo bonito
-(add-to-list 'default-frame-alist '(font . "Hack"))
-(set-face-attribute 'default nil :family "Hack")
-(set-default-font "Hack 11")
 
-;;(set-face-attribute 'default nil :height 168)
 (tooltip-mode -1)
 (tool-bar-mode -1)
 (set-window-fringes nil 0 0)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-;; (toggle-frame-fullscreen)
 (setq inhibit-startup-screen t)
 (display-time-mode 1)
-(setq ring-bell-function 'ignore)
-;;(shell-command-to-string "echo -n $(date +%k:%M--%m-%d)")
 (display-battery-mode 1)
+(electric-pair-mode 1)
+(show-paren-mode 1)
+(setq custom-file "~/.emacs.d/.emacs-custom.el")
+(setq make-backup-files nil)
+(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+
 (use-package rainbow-delimiters
   :ensure t
   :init
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode 1))
+
 (use-package telephone-line
-  :ensure t) ;; Bueno, esto necesita un repaso gordo. Espero que mole mucho.
+  :ensure t)
 (setq telephone-line-subseparator-faces '())
 (setq telephone-line-height 24
       telephone-line-evil-use-short-tag t)
 (telephone-line-mode t)
-;; Tema
+
 (use-package doom-themes
   :ensure t
   :init (load-theme 'doom-molokai t)
   :config (doom-themes-org-config)
-          (doom-themes-neotree-config))
-;; Unas cuantas utilidades para mejorar nuestra experiencia
+  (doom-themes-neotree-config))
+
+(add-to-list 'default-frame-alist '(font . "Hack 10"))
+(set-face-attribute 'default nil :family "Hack 10")
+(set-default-font "Hack 10")
+
 (use-package windmove
   :ensure t
-  :bind (("M-<up>" . windmove-up)
-	 ("M-<down>" . windmove-down)
-	 ("M-<right>" . windmove-right)
-	 ("M-<left>" . windmove-left)))
-(use-package ido
-  :init (ido-mode))
-(use-package neotree
-  :ensure t
-  :bind (("C-x n" . neotree-toggle)))
+  :bind (("C-<up>" . windmove-up)
+	 ("C-<down>" . windmove-down)
+	 ("C-<right>" . windmove-right)
+	 ("C-<left>" . windmove-left)))
 
+;; Thanks to @Ironjanowar for helm config
 (use-package helm
+  :ensure t
   :init (helm-mode 1)
-  :config (require 'helm-config)
+  :config (require 'helm-config
+	   (setq helm-split-window-in-side-p t
+		 helm-buffers-fuzzy-matching t 
+		 helm-recentf-fuzzy-match t
+		 helm-move-to-line-cycle-in-source t)
+	   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action))
   :bind (("C-x C-f" . helm-find-files)
-	 ("M-x" . helm-M-x)))
-(use-package ac-helm
-  :ensure t)
-;;(use-package auto-complete
-;;  :ensure t
-;;  :config (ac-config-default))
+	 ("M-x" . helm-M-x)
+	 ("C-x b" . helm-mini)
+	 ("C-x C-b" . helm-buffers-list)
+	 ("C-c g" . helm-google-suggest)))
 
-(use-package company
-  :ensure t
-  :init
-  :config (add-hook 'after-init-hook 'global-company-mode))
-
-;; Varios Básicos
-(electric-pair-mode 1)
-(show-paren-mode 1)
-(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
-
-;; Juegos :D
-(use-package tetris
-  :ensure t
-  :bind ("C-t" . tetris))
 (use-package typing
-  :ensure t )
-(use-package poker
-  :ensure t )
-;; Accesorios varios
-(use-package zone-rainbow
-  :ensure t
-  :bind ("C-z" . zone-rainbow))
+  :ensure t)
+
 (use-package spotify
   :ensure t
   :bind(("C-S-s p" . spotify-playpause)
 	("C-S-s n" . spotify-next)
 	("C-S-s b" . spotify-previous)
 	("C-S-s c" . spotify-current)))
+
 (use-package golden-ratio
   :ensure t
   :config (golden-ratio-mode t))
-(use-package rainbow-mode
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package neotree
   :ensure t
-  :config (rainbow-mode t))
-;; Modos para programar (Aunque no hay nada de programación aquí xD) y demás
-(use-package markdown-mode+
-  :ensure t)
-(use-package flymd
-  :ensure t)
+  :bind (("C-x n" . neotree-toggle))
+  )
+
+(use-package undo-tree
+  :ensure t
+  :config (global-undo-tree-mode t))
+
+(use-package zone-rainbow
+  :ensure t
+  :bind ("C-z" . zone-rainbow))
+
 (use-package magit
   :ensure t)
-;;(use-package auctex
-;;  :ensure t)
-;;  :config ((setq TeX-auto-save t)
-;;	   (setq TeX-parse-self t)))
-(put 'upcase-region 'disabled nil)
+
